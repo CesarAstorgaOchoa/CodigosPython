81136bf4083e744c4fa01e5d509eae99e8a8710c
CesarAstorgaOchoa <18170276@itculiacan.edu.mx>
2022-10-02 13:06:46 -0600

    primer commit

---
+++ Fundamentos.py
@@ -0,0 +1,28 @@
+print("Hola mundo")
+print('Hola mundo')
+
+#Comentario de una linea 
+
+"""
+Comentarios 
+en 
+multiples
+lineas
+"""
+
+'''
+Otros comentarios 
+en 
+multiples
+lineas
+'''
+
+#Consultas de tipo de dato
+#type
+print(type("Hola")) #str
+print(type(1))      #int
+print(type(1.2))    #float
+print(type(True))   #bool
+print(type(1+2j))   #complex
+
+
