ac3c2937643a8298d69be870eb8ecd16b71b9ff7
CesarAstorgaOchoa <18170276@itculiacan.edu.mx>
2022-10-02 18:40:51 -0600

    segundo commit

--- Fundamentos.py
+++ Fundamentos.py
@@ -26,3 +26,12 @@ print(type(True))   #bool
 print(type(1+2j))   #complex
 
 
+#Variables
+
+my_int_variable = 5
+my_bool_variable = true 
+my_string_variable="Hola"
+
+print(my_bool_variable)
+print(my_int_variable)
+print(my_string_variable)
